6004cfeb3e6248fce619c3de0c4d0b29c57397bb
Michael Case <michael085@revature.net>
2026-05-15 10:53:47 -0400

    Fixed tests

--- frontend/src/tests/pages/ProductDetails.test.tsx
+++ frontend/src/tests/pages/ProductDetails.test.tsx
@@ -1,8 +1,14 @@
-import { describe, it, expect } from "vitest"
+import { describe, it, expect, vi, beforeEach, afterEach } from "vitest"
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter, Route, Routes } from "react-router-dom"
 import ProductDetails from "../../pages/ProductDetails"
+import { useState } from "react"
 
+
+afterEach(() =>
+{
+    vi.restoreAllMocks();
+})
 
 
 
@@ -10,8 +16,20 @@ describe('<ProductDetails />', () =>
 {
     // these will need to change when fakeProducts from ProductDetails.tsx is removed.
     // for now, these tests will be functioning on the assumption that fakeProducts exists
-    it('shows the proper product based on the path', () =>
+    it('shows the proper product based on the path', async () =>
     {
+
+
+        vi.stubGlobal('fetch', vi.fn().mockResolvedValue({
+            ok: true,
+            json: async () => ({
+                id: 1,
+                name: "Laptop",
+                price: 500.00,
+                categoryName: "categoryName",
+                stockQuantity: 5
+            })
+        }))
 
         render(
             <MemoryRouter initialEntries={["/1"]}>
@@ -21,7 +39,8 @@ describe('<ProductDetails />', () =>
             </MemoryRouter>
         )
 
-        expect(screen.getByText('Laptop')).toBeInTheDocument();
+
+        expect(await screen.findByText('Laptop')).toBeInTheDocument();
         expect(screen.getByText('$500.00')).toBeInTheDocument();
         expect(screen.getByText('Add To Bag')).toBeInTheDocument();
         expect(screen.getByText('Add To Bag')).toHaveRole('button');
@@ -31,7 +50,11 @@ describe('<ProductDetails />', () =>
     it('shows "Product not found" when there are no products to display', () =>
     {
         render(
-            <ProductDetails />
+            <MemoryRouter initialEntries={["/1"]}>
+                <Routes>
+                    <Route path=":id" element={<ProductDetails />} />
+                </Routes>
+            </MemoryRouter>
         )
 
         expect(screen.getByText('Product not found')).toBeInTheDocument();
